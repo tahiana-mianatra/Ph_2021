cc82eabb7c822eb510b96832f189b2238ba889c6
tahiana-mianatra <andry.tahiana@yahoo.fr>
2025-10-07 13:10:19 +0300

    All type of awareness done but we got to combine them into one df

--- 01_Script/01_Cleaning.R
+++ 01_Script/01_Cleaning.R
@@ -14,7 +14,41 @@ survey <- gross_data %>%
     .cols = D1A ,
     .fns = ~ replace(.x, .x == 11, NA)                
   ))
-  
-#Step 2 : preparing brand awareness long format
+#Step 2: removing 99 from B1A; B1B
+survey <- survey %>%
+  mutate(across(
+    .cols = c(B1A, B1B) ,
+    .fns = ~replace(.x , x == 99, NA)
+  ))
+#Step 3 : preparing brand awareness long format
 aware <- survey %>%
-  select(QUEST, )
+  select(QUEST,B1A, B1B)
+tom <- aware %>%
+  filter(!is.na(B1A) & B1A != "") %>%
+  transmute(QUEST, brand = B1A , type = "Top of Mind" )
+
+#Spontaneous
+spont <- aware %>%
+  filter(!is.na(B1B)) %>%
+  separate_rows(B1B, sep = "-") %>%
+  filter(!is.na(B1B) & B1B =="") %>%
+  transmute(QUEST, brand = B1B, type = "Spontaneous" ) %>%
+  mutate(
+    brand = as.numeric(brand)
+  )
+ #Assisted awareness
+assisted <- survey %>%
+  select(QUEST, starts_with("B2_")) %>%
+  pivot_longer(
+    cols = starts_with("B2_"),
+    names_to = "brand_code" ,
+    values_to = "awareness"
+  ) %>%
+  filter( awareness == 1) %>%
+  mutate(
+    brand = as.numeric(str_remove(brand_code, "B2_")),
+    type = "Assisted"
+  ) %>%
+  select(QUEST, brand, type)
+  
+  
