f6243ee3716a279b4fae26e9b3eb71741faa9459
tahiana-mianatra <andry.tahiana@yahoo.fr>
2025-10-08 12:06:08 +0300

    First commit, only comments on how we will do it

---
+++ 01_Script/03_Market_penetration_rate.R
@@ -0,0 +1,7 @@
+#Input: Cleaned dataframe
+#Output: Market penetration rate barplot
+#Load library
+#Load data
+#Select the relevant variable (QUEST, C1, C15, C16)
+#Prepare the data for plotting
+#Plot
